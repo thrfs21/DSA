3c4b14cb78e412bde1addcd16976c0a284a73844
Taher Sanawadwala <tahersannu@gmail.com>
2026-08-24 22:40:45 +0530

    23. Merge k Sorted Lists

---
+++ 23. Merge k Sorted Lists.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<
+            pair<int, ListNode*>,
+            vector<pair<int, ListNode*>>,
+            greater<pair<int, ListNode*>>
+        > pq;
+
+        for(auto head : lists) {
+            if(head)
+                pq.push({head->val, head});
+        }
+
+        ListNode* dummy = new ListNode(0);
+        ListNode* temp = dummy;
+
+        while(!pq.empty()) {
+            auto [val, node] = pq.top();
+            pq.pop();
+
+            temp->next = node;
+            temp = temp->next;
+
+            if(node->next) {
+                pq.push({node->next->val, node->next});
+            }
+        }
+
+        return dummy->next;
+    }
+};
