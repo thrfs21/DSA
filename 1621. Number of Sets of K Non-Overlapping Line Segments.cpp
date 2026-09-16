3693c30e0072e31e08aaa0df2711cc456b18688c
Taher Sanawadwala <tahersannu@gmail.com>
2026-09-16 19:22:58 +0530

    1621. Number of Sets of K Non-Overlapping Line Segments

---
+++ 1621. Number of Sets of K Non-Overlapping Line Segments.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+
+    int numberOfSets(int n, int k) {
+        vector<vector<long long>> dp(n, vector<long long>(k+1));
+
+        for(int i=0; i<n; i++) dp[i][0] = 1;
+
+        for(int j=1; j<=k; j++){
+            long long sum = 0;
+            for(int i=1; i<n; i++){
+                sum = (sum + dp[i-1][j-1]) % MOD;
+                dp[i][j] = (dp[i-1][j] + sum) % MOD;
+            }
+        }
+
+        return dp[n-1][k];
+    }
+};
